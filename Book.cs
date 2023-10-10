890db2b9ee88a23f9ee03fb68d9e23a39ee9506c
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-10 10:55:05 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    class Book
+        {
+            public string title;
+            public string author;
+            public int pages;
+          
+           
+                public Book( string aTitle, string aAuthor,int aPages)
+            {
+                title = aTitle;
+                author = aAuthor;   
+                pages = aPages;
+               
+            }
+
+            /*static void Main(string[] args)
+            {
+                Book book1 = new Book();
+                book1.title = "Bengilithi Lizokuna";
+                book1.author = "Benjamin Nkosi";
+                book1.pages = 400;
+               
+
+                Book book2 = new Book();
+                book2.title = "Long Wark to Freedom";
+                book2.author = "Nelson Mandela";
+                book2.pages = 784;
+             
+
+                Console.WriteLine(book1.pages);
+                Console.WriteLine(book2.title);
+
+                Console.ReadLine();
+
+            }*/
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace _18.Constructors
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Benjamin Nkisi" , "Bengilithi Lizokuna",400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine(); 
+
+            
+        }
+    }
+}
